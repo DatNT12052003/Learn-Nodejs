461bc23de9c3787f11838000a16c4f7ebfbbbbdb
DatNT12052003 <thanhdat2003pt@gmail.com>
2024-11-03 13:16:49 +0700

    CRUD part 3

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import db from '../models/index'
+import { where } from 'sequelize';
 
 
 const salt = bcrypt.genSaltSync(10);
@@ -10,13 +11,13 @@ let createNewUser = async (data) => {
             let hashPasswordFromBcrypt = await hashUserPassword(data.password);
             await db.User.create({
                 email: data.email,
-    password: hashPasswordFromBcrypt,
-    firstName: data.firstName,
-    lastName: data.lastName,
-    address: data.address,
-    phonenumber: data.phonenumber,
-    gender: data.gender === '1' ? true : false,
-    roleId: data.roleId,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId,
             })
 
             resolve('ok create a new user succeed')
@@ -32,7 +33,7 @@ let hashUserPassword = (password) => {
         try {
             let hashPassword = await bcrypt.hashSync(password, salt);
             resolve(hashPassword)
-        } catch(e) {
+        } catch (e) {
             reject(e);
         }
     })
@@ -51,7 +52,50 @@ let getAllUser = () => {
     })
 }
 
+let getUserInfoById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId },
+                raw: true,
+            })
+            if (user) {
+                resolve(user)
+            } else {
+                resolve({})
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: data.id }
+            })
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+
+                await user.save();
+                
+                let allUsers = await db.User.findAll();
+                resolve(allUsers);
+            } else {
+                resolve();
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    })
+}
 module.exports = {
     createNewUser: createNewUser,
     getAllUser: getAllUser,
+    getUserInfoById: getUserInfoById,
+    updateUserData: updateUserData,
 }
